2d307cd6e99c773b74ac2fa5802d37a3e4a3d038
Tom <tomfokker2000@hotmail.com>
2018-09-25 09:44:34 +0200

    Deployment script en documentatie zijn toegevoegd

--- EindopdrachtServersideProgrammingTomFokker/BlobSASToken.cs
+++ EindopdrachtServersideProgrammingTomFokker/BlobSASToken.cs
@@ -9,38 +9,22 @@ namespace EindopdrachtServersideProgrammingTomFokker
 {
     class BlobSASToken
     {
-        public static string GetBlobSasUri(CloudBlobContainer container, string blobName, string policyName = null)
+        public static string GetBlobSasUri(CloudBlobContainer container, string blobName)
         {
             string sasBlobToken;
 
-            // Get a reference to a blob within the container.
-            // Note that the blob may not exist yet, but a SAS can still be created for it.
+            // Get blob reference
             CloudBlockBlob blob = container.GetBlockBlobReference(blobName);
 
-            if (policyName == null)
-            {
-                // Create a new access policy and define its constraints.
-                // Note that the SharedAccessBlobPolicy class is used both to define the parameters of an ad-hoc SAS, and
-                // to construct a shared access policy that is saved to the container's shared access policies.
-                SharedAccessBlobPolicy adHocSAS = new SharedAccessBlobPolicy()
-                {
-                    // When the start time for the SAS is omitted, the start time is assumed to be the time when the storage service receives the request.
-                    // Omitting the start time for a SAS that is effective immediately helps to avoid clock skew.
-                    SharedAccessExpiryTime = DateTime.UtcNow.AddHours(24),
-                    Permissions = SharedAccessBlobPermissions.Read
-                };
+            // Create access policy
+            SharedAccessBlobPolicy adHocSAS = new SharedAccessBlobPolicy();
+            adHocSAS.SharedAccessExpiryTime = DateTime.UtcNow.AddHours(24);
+            adHocSAS.Permissions = SharedAccessBlobPermissions.Read;
 
-                // Generate the shared access signature on the blob, setting the constraints directly on the signature.
-                sasBlobToken = blob.GetSharedAccessSignature(adHocSAS);
-            }
-            else
-            {
-                // Generate the shared access signature on the blob. In this case, all of the constraints for the
-                // shared access signature are specified on the container's stored access policy.
-                sasBlobToken = blob.GetSharedAccessSignature(null, policyName);
-            }
+            // Create SAS token for blob
+            sasBlobToken = blob.GetSharedAccessSignature(adHocSAS);
 
-            // Return the URI string for the container, including the SAS token.
+            // Return SAS token
             return sasBlobToken;
         }
     }
